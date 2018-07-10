5a674356d181201a2321f9bd2348f3a8a894b26d
DaJuukes <dajuukes@gmail.com>
2018-07-10 18:37:55 -0500

    begin room support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const initServer = require('./src/initServer.js')
 const conn = initServer()
 
 let users = new Map()
+let invites = new Map()
 
 conn.use(function (socket, next) {
   var handshakeData = socket.request
@@ -24,7 +25,30 @@ conn.on('connection', (socket) => {
   socket.on('message', (data) => {
     const sender = users.get(socket.id)
     console.log(sender + ': ' + data.message)
-    socket.broadcast.emit('chat_message', { username: sender, message: data.message })
+    if (data.room) {
+      if (conn.rooms[data.room] && conn.rooms[data.room].includes(socket.id)) {
+        socket.broadcast.to(data.room).emit('chat_message', { username: sender, message: data.message })
+      } else {
+        socket.emit('fail', { message: 'room is invalid' })
+      }
+    } else {
+      socket.broadcast.emit('chat_message', { username: sender, message: data.message })
+    }
+  })
+
+  socket.on('adduser', () => {
+    // Add user to room
+    
+  })
+
+  socket.on('joinroom', (data) => {
+    const sender = users.get(socket.id)
+    if (socket.rooms.includes(data.room) && invites.has(sender)) {
+      socket.join(data.room)
+      socket.emit('success', { message: 'joined room successfully' })
+    } else {
+      socket.emit('fail', { message: 'room is invalid' })
+    }
   })
 
   socket.on('disconnect', () => {
